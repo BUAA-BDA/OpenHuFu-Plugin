8d66efadfd8c56a288349dc56342c8272baf1a10
SongY123 <121220087@smail.nju.edu.cn>
2023-03-31 00:14:57 +0800

    change GITHUB_TOKEN for deploying openhufu-aby

--- package.sh
+++ package.sh
@@ -9,5 +9,5 @@ rm -rf build && mkdir build && cd build
 cmake .. && make -j 8
 cd ../..
 cp swig/build/lib/* ${LD_LIBRARY_PATH}
-mvn clean install -T 0.5C -Dmaven.test.skip=true
+mvn clean install -T 0.5C -DskipTests
 cp target/*.jar ${LD_LIBRARY_PATH}/aby4j.jar
